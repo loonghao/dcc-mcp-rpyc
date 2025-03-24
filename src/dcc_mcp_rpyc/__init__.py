8cce762ce13ee75bcf1050605df1603dba66320d
longhao <hal.long@outlook.com>
2025-03-25 01:19:50 +0800

    refactor: move service factory functions to server module
    - Move create_service_factory, create_shared_service_instance, and get_rpyc_config from parameters.py to server.py
    - Update imports in __init__.py and example code
    - Add documentation for service factories and parameter handling in README
    - Improve code organization with better separation of concerns
    
    Signed-off-by: longhao <hal.long@outlook.com>

--- src/dcc_mcp_rpyc/__init__.py
+++ src/dcc_mcp_rpyc/__init__.py
@@ -8,17 +8,17 @@ from dcc_mcp_rpyc import server
 from dcc_mcp_rpyc.adapter import DCCAdapter
 from dcc_mcp_rpyc.client import BaseDCCClient
 from dcc_mcp_rpyc.client import ConnectionPool
-from dcc_mcp_rpyc.parameters import create_service_factory
-from dcc_mcp_rpyc.parameters import create_shared_service_instance
 from dcc_mcp_rpyc.parameters import deliver_parameters
 from dcc_mcp_rpyc.parameters import execute_remote_command
-from dcc_mcp_rpyc.parameters import get_rpyc_config
 from dcc_mcp_rpyc.parameters import process_rpyc_parameters
 from dcc_mcp_rpyc.server import BaseRPyCService
 from dcc_mcp_rpyc.server import DCCRPyCService
 from dcc_mcp_rpyc.server import DCCServer
 from dcc_mcp_rpyc.server import create_dcc_server
 from dcc_mcp_rpyc.server import create_raw_threaded_server
+from dcc_mcp_rpyc.server import create_service_factory
+from dcc_mcp_rpyc.server import create_shared_service_instance
+from dcc_mcp_rpyc.server import get_rpyc_config
 
 __all__ = [
     "BaseDCCClient",
